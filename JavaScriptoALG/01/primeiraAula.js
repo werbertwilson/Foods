5a9d222284a3455401ec9589440ebadd9af3e1e6
Werbert Wilson S. C. da Conceição <77551645+werbertwilson@users.noreply.github.com>
2023-04-27 00:15:58 -0300

    treinojavascrip42720231215
    Revisão de javascript, variaveis e tipo

---
+++ JavaScriptoALG/01/primeiraAula.js
@@ -0,0 +1,37 @@
+var nome = "werbert";
+var idade = 30;
+var comida;
+comida = "macarrão";
+var carro = "celta";
+var moto = "yamaha";
+var bebida = "suco";
+var combustivel = "gasolina";
+var animal = "sapo";
+
+console.log(
+  nome,
+  idade,
+  comida,
+  comida,
+  carro,
+  moto,
+  bebida,
+  combustivel,
+  animal
+);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//-------------------------- exec 2
+
+var localTrabalho = "FIESC";
+var meuNome = "werbert";
+var numString = "werbert" + 30;
+var minhaidade = 30;
+var nomeCompleto = meuNome + " wilson";
+var ingles = "it's time";
+
+console.log(nomeCompleto, ingles);
+var nomeTemplate = `${meuNome} ${idade}`;
+console.log(nomeTemplate);
